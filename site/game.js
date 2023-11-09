dfb4bf463388c59ab34c34126e8460f6a0f40b5a
AndyStubbs <astubbs50@gmail.com>
2023-11-09 15:11:49 -0700

    Added player movement

--- site/game.js
+++ site/game.js
@@ -3,6 +3,9 @@
 ( function () {
 
 	const game = {};
+	const MAX_VELOCITY_Y_FOR_GROUNDED = 1.8;
+	const JUMP_FORCE = -0.25;
+	const DEBUG = true;
 
 	g.loadLevel = async function ( name ) {
 		if( !game.tiles ) {
@@ -19,6 +22,10 @@
 			return;
 		}
 
+		game.lastBodyId = 0;
+		game.bodies = [];
+		game.actorsMap = {};
+
 		// Create the level container.
 		if( game.container ) {
 			game.app.stage.removeChild( game.container );
@@ -29,7 +36,6 @@
 
 		// Create the physics world.
 		game.engine = Matter.Engine.create();
-		game.bodies = [];
 
 		// Create the level objects.
 		game.level.layers.forEach( layer => {
@@ -50,7 +56,13 @@
 		Matter.Runner.run( game.runner, game.engine );
 
 		// Run the graphics step.
-		g.app.ticker.add( graphicsStep );
+		g.app.ticker.add( step );
+
+		// Setup the collider event
+		Matter.Events.on( game.engine, "collisionStart", collisionCheck );
+
+		// Setup the input handlers.
+		setupInputs();
 	};
 
 	async function loadTiles() {
@@ -100,6 +112,7 @@
 
 		// Create the player sprite
 		player.sprite = new PIXI.Sprite( g.spritesheet.textures[ "p1_front.png" ] );
+		player.sprite.anchor.set( 0.5, 0.5 );
 		const pos = game.container.toLocal( new PIXI.Point( obj.x, obj.y ) );
 		player.sprite.x = pos.x;
 		player.sprite.y = pos.y;
@@ -110,10 +123,28 @@
 		// Create the player physics body
 		const width = player.sprite.width / player.sprite.scale.x;
 		const height = player.sprite.height / player.sprite.scale.y;
-		player.body = Matter.Bodies.rectangle( obj.x, obj.y, width, height );
+		player.body = Matter.Bodies.rectangle(
+			obj.x,
+			obj.y,
+			width,
+			height,
+			{
+				"inertia": Infinity,
+				"customData": { "type": "actor" }
+			}
+		);
+		game.lastBodyId++;
+		game.actorsMap[ player.body.id ] = player;
 		game.bodies.push( player.body );
 
+		// Assign the player to the game object.
 		game.player = player;
+
+		if( DEBUG ) {
+			// Add a debug graphics object to the sprite.
+			player.sprite.debug = new PIXI.Graphics();
+			game.container.addChild( player.sprite.debug );
+		}
 	}
 
 	function createGround( obj ) {
@@ -122,26 +153,114 @@
 			obj.y + obj.height / 2,
 			obj.width,
 			obj.height,
-			{ "isStatic": true }
+			{
+				"isStatic": true,
+				"customData": {
+					"type": "ground"
+				}
+			}
 		);
+		body.friction = 0;
 		game.bodies.push( body );
 	}
 
-	function graphicsStep() {
+	function step() {
 		updateSpritePosition( game.player.sprite, game.player.body );
-	}
 
-	function physicsStep() {
-		game.world.step();
-		console.log( Date.now() );
+		// Apply player movement for platformer controls.
+		const player = game.player;
+		const keys = game.keys;
+		const speed = 5;
+		if( keys.ArrowLeft ) {
+			Matter.Body.translate( player.body, { "x": -speed, "y": 0 } );
+		}
+		if( keys.ArrowRight ) {
+			Matter.Body.translate( player.body, { "x": speed, "y": 0 } );
+		}
+		if( keys.ArrowUp ) {
+
+			// Don't allow player to jump if falling, allow for a little bit of leeway.
+			if( player.isGrounded && player.body.velocity.y > MAX_VELOCITY_Y_FOR_GROUNDED ) {
+				player.isGrounded = false;
+			}
+			if( player.isGrounded ) {
+				Matter.Body.applyForce(
+					player.body, player.body.position, { "x": 0, "y": JUMP_FORCE }
+				);
+				player.isGrounded = false;
+			}
+		}
 	}
 
 	function updateSpritePosition( sprite, body ) {
 		const pos = game.container.toLocal(
-			new PIXI.Point( body.position.x, body.position.y - 46 )
+			new PIXI.Point( body.position.x, body.position.y )
 		);
 		sprite.x = pos.x;
 		sprite.y = pos.y;
+
+		// Update the sprite rotation.
+		sprite.rotation = body.angle;
+		
+		// Draw a wire frame around the sprite using the body
+		// vertices.
+		if( sprite.debug ) {
+			sprite.debug.clear();
+			sprite.debug.lineStyle( 1, "#00ff00" );
+			sprite.debug.beginFill( "#000000", 0 );
+			let pos = game.container.toLocal(
+				new PIXI.Point( body.vertices[ 0 ].x, body.vertices[ 0 ].y )
+			);
+			sprite.debug.moveTo( pos.x, pos.y );
+			for( let i = 1; i < body.vertices.length; i++ ) {
+				pos = game.container.toLocal(
+					new PIXI.Point( body.vertices[ i ].x, body.vertices[ i ].y )
+				);
+				sprite.debug.lineTo( pos.x, pos.y );
+			}
+			pos = game.container.toLocal(
+				new PIXI.Point( body.vertices[ 0 ].x, body.vertices[ 0 ].y )
+			);
+			sprite.debug.lineTo( pos.x, pos.y );
+			sprite.debug.endFill();
+		}
+	}
+
+	function setupInputs() {
+		const keys = {};
+		document.addEventListener( "keydown", keydown );
+		document.addEventListener( "keyup", keyup );
+		game.keys = keys;
+	}
+
+	function clearInputs() {
+		document.removeEventListener( "keydown", keydown );
+		document.removeEventListener( "keyup", keyup );
+		game.keys = {};
+	}
+
+	function keydown( e ) {
+		game.keys[ e.key ] = true;
+	}
+
+	function keyup( e ) {
+		game.keys[ e.key ] = false;
+	}
+
+	function collisionCheck( collisions ) {
+		const pairs = collisions.source.pairs.list;
+		for( let i = 0; i < pairs.length; i++ ) {
+			const pair = pairs[ i ];
+			const a = pair.bodyA.customData;
+			const b = pair.bodyB.customData;
+			if( Math.abs( pair.collision.penetration.y ) > 0 ) {
+				if( a.type === "actor" && b.type === "ground" ) {
+					game.actorsMap[ pair.bodyA.id ].isGrounded = true;
+				} else if ( a.type === "ground" && b.type === "actor" ) {
+					game.actorsMap[ pair.bodyB.id ].isGrounded = true;
+				}
+			}
+		}
 	}
 
 } )();
